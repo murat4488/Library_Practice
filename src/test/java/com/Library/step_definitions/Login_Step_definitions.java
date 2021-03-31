24fd4f338e540241374e9d445e526861b1353f77
Murat Guney <muratguney@msn.com>
2021-03-31 11:25:37 -0400

    created new project

---
+++ src/test/java/com/Library/step_definitions/Login_Step_definitions.java
@@ -0,0 +1,43 @@
+package com.Library.step_definitions;
+
+import io.cucumber.java.en.*;
+
+public class Login_Step_definitions {
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+
+    }
+    @When("user enter librarian password")
+    public void user_enter_librarian_password() {
+
+    }
+    @Then("user should see dashboard")
+    public void user_should_see_dashboard() {
+
+    }
+
+
+    @When("user enters student username")
+    public void user_enters_student_username() {
+
+    }
+    @When("user enters student password")
+    public void user_enters_student_password() {
+
+    }
+
+    @When("user enters admin username")
+    public void user_enters_admin_username() {
+
+    }
+    @When("user enters admin password")
+    public void user_enters_admin_password() {
+
+    }
+
+}
